0f81d1084dae478452234a9a0e00715610c49595
Gordana Minovska <ekogoca@gmail.com>
2017-03-18 14:54:23 -0700

    Fix glitch

--- JS&CSS Clock/script.js
+++ JS&CSS Clock/script.js
@@ -11,13 +11,28 @@ var secondsDegrees = 6 * seconds; // 360/60 = 6
 var minuteDegrees = 6 * minutes;
 var hourDegrees = 30 * hours;
 
+var allHands = document.querySelector('.hand');
 var secondsHand = document.querySelector('.second-hand');
 var minuteHand = document.querySelector('.min-hand');
 var hourHand = document.querySelector('.hour-hand');
 
+
 secondsHand.style.transform = `rotate(${90 + secondsDegrees}deg)`;
 minuteHand.style.transform = `rotate(${90 + minuteDegrees}deg)`;
 hourHand.style.transform = `rotate(${90 + hourDegrees}deg)`;
+
+/*
+When the rotation degrees go from (6 * 59)449 to (6 * 0)90 
+there is a glitch due to the transition property, 
+the hand goes a full circle backwards.
+this removes the transition property on 90 degrees
+*/
+
+if(seconds === 0) {
+secondsHand.style.transition = "all 0s";
+}else {
+secondsHand.style.transition = "";
+}
 }
 
 setInterval(getTime, 1000);
